31e7beedcb88238378f9cce38e7857982e631184
jihun <gn753@naver.com>
2023-05-23 15:29:37 +0900

    로그인 토큰 에러 수정

--- features/CampLogDetails/service/fetchBookmarks.ts
+++ features/CampLogDetails/service/fetchBookmarks.ts
@@ -2,7 +2,6 @@ import { axiosSetting } from "api/api";
 
 const fetchBookmarks = async (memberId: any) => {
   const response = await axiosSetting.get(`/api/bookmark/${memberId}`);
-  console.log("add");
   return response;
 };
 export default fetchBookmarks;
